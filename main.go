ceea69d6e2a561134fc377c99b0258132a52de30
Alec Missine <alec_missine@yahoo.com>
2020-04-23 10:16:08 -0400

    Add an unrelated flag 'mode' to play with

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import "flag"
 import "fmt"
 import "github.com/amissine/golib/statemachine"
-import "github.com/golang/glog"
+
+//import "github.com/golang/glog"
 import "os"
 
 type StateObj struct{}
@@ -16,17 +17,21 @@ func (p *StateObj) PrintWorld() (statemachine.StateFn, error) {
 	fmt.Println("World")
 	return nil, nil
 }
-func main() {
+func main() { // if using glog, run with 'go run main.go -logtostderr=true'
+	// TODO remove the unrelated stuff:
+	mode := flag.String("mode", "sim", "default is 'sim'; set 'trade' if you do not want to simulate")
+
 	so := &StateObj{}
 	log := func(s string, i ...interface{}) {
-		glog.Infof(s, i...)
+		//glog.Infof(s, i...)
+		fmt.Printf(s+"\n", i...)
 	}
-	exec := statemachine.New("helloWorld", so.PrintHello, statemachine.LogFacility(log))
+	exec := statemachine.New("helloworld", so.PrintHello, statemachine.LogFacility(log))
 	exec.Log(true)
-	if len(os.Args) > 1 {
-		flag.Parse()
-		log("logger", os.Args)
-	}
+	//if len(os.Args) > 1 {
+	flag.Parse()
+	log("logger %v %s\n", os.Args, *mode) // TODO remove the unrelated stuff
+	//}
 
 	if e := exec.Execute(); e != nil {
 		fmt.Printf("\nERROR: %s\n\n", e)
